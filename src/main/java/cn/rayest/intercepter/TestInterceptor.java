8af3944c00d9eea7347b73b1e7f6e89921b6954b
Rayest <yi@rayest.cn>
2016-09-11 11:55:03 +0800

    设置拦截器对乱码进行处理

--- src/main/java/cn/rayest/intercepter/TestInterceptor.java
+++ src/main/java/cn/rayest/intercepter/TestInterceptor.java
@@ -10,7 +10,8 @@ import javax.servlet.http.HttpServletResponse;
  * Created by Rayest on 2016/9/11 0011.
  */
 public class TestInterceptor implements HandlerInterceptor {
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+        request.setCharacterEncoding("utf-8");
         System.out.println("执行了 preHandle() 方法...");
         return true;
     }
