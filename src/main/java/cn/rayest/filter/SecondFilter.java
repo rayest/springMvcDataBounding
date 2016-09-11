7df393160218902423e258708d031c25620aef4f
Rayest <yi@rayest.cn>
2016-09-12 00:36:28 +0800

    过滤器链

--- src/main/java/cn/rayest/filter/SecondFilter.java
+++ src/main/java/cn/rayest/filter/SecondFilter.java
@@ -7,18 +7,18 @@ import java.io.IOException;
 /**
  * Created by Rayest on 2016/9/11 0011.
  */
-public class FirstFilter implements Filter {
+public class SecondFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("init()...FirstFilter");
+        System.out.println("init()...SecondFilter");
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Start...doFilter...FirstFilter");
+        System.out.println("Start...doFilter...SecondFilter");
         filterChain.doFilter(request, response);
-        System.out.println("End...doFilter...FirstFilter");
+        System.out.println("End...doFilter...SecondFilter");
     }
 
     public void destroy() {
-        System.out.println("destroy()...FirstFilter");
+        System.out.println("destroy()...SecondFilter");
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+This is my JSP page.
+<%
+    System.out.println("处理过程...");
+%>
+
+</body>
+</html>
